c4e244227d1ea081235116c83ae1cbbac59e4223
Drew Waddell <drew.waddell@gmail.com>
2021-04-14 12:34:56 -0400

    Corrected comments
    Nothing special here

--- BuildFiles/PostBuild.bat
+++ BuildFiles/PostBuild.bat
@@ -1,4 +1,4 @@
-
+:: Echo the parameters we recieved 
 echo %1
 echo %2
 
@@ -8,5 +8,5 @@ echo F|xcopy /i /y %2BuildFiles\LICENSE %1..\LICENSE
 :: Copy Manifest File
 echo F|xcopy /i /y %2BuildFiles\manifest.json %1..\manifest.json
 
-:: Copy Constans File
+:: Copy Constants File
 echo F|xcopy /i /y %2BuildFiles\constants.xlf %1..\Resources\constants.xlf
