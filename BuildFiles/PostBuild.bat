3da538c1feec9cb1799e1b0997b061ee4ad1d353
Drew Waddell <drew.waddell@gmail.com>
2021-04-14 12:26:32 -0400

    Created PostBuild.bat
    Created a batch file to handle some file management between a Debug and Release build of the solution.
    
    Added call "$(ProjectDir)BuildFiles\PostBuild.bat" "$(TargetDir)" "$(ProjectDir)" as a Post-Build Event.

---
+++ BuildFiles/PostBuild.bat
@@ -0,0 +1,12 @@
+
+echo %1
+echo %2
+
+:: Copy License File
+echo F|xcopy /i /y %2BuildFiles\LICENSE %1..\LICENSE
+
+:: Copy Manifest File
+echo F|xcopy /i /y %2BuildFiles\manifest.json %1..\manifest.json
+
+:: Copy Constans File
+echo F|xcopy /i /y %2BuildFiles\constants.xlf %1..\Resources\constants.xlf
